fbc482b18dfb08ccb28c6b4e2e30c5e2c902841f
REsearch09 <133530256+REsearch09@users.noreply.github.com>
2023-05-20 23:14:20 +0300

    Update passwordgeneration.py

--- passwordgeneration.py
+++ passwordgeneration.py
@@ -3,6 +3,9 @@ import random
 character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 password_lenght = int(input("Parolanın uzunluğunu söyleyiniz."))
 password = ""
-for i in range(password_lenght):
-    password += random.choice(character)
-print(password)
+if password_lenght >= 6:
+    for i in range(password_lenght):
+        password += random.choice(character)
+    print(password)
+else:
+    print("Lütfen en az 6 haneli bi şifre seçiniz")
